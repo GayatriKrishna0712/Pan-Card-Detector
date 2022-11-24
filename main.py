fea322f2d8b82b8c3dda5fe0373709da929d6c16
Gayatri Krishna <93417245+GayatriKrishna0712@users.noreply.github.com>
2022-11-24 20:56:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+import cv2  # computer vision for all the image processing requirements
+from PIL import Image
+import requests # used for fetching data from the URLs as we are going to take images from various urls
+from skimage.metrics import structural_similarity  # used to determine the structural similarity score of the original and tamppered image
+import imutils  # helps to grab the contours
